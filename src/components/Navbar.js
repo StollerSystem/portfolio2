0d108536e2070be6a97b3408eb98f846e7e6e749
Ben Stoller <stollersystemsound@gmail.com>
2021-01-06 20:46:04 -0800

    add css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 
 function Navbar() {
   
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton);
+
   return (
     <>
-      <nav classNmae="navbar">
+      <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
             TRVL <i className='fab fa-typo3' />
@@ -41,6 +54,7 @@ function Navbar() {
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle='btn--outline'> SIGN UP </Button>}
         </div>
       </nav>
     </>
