4a56cfda5807b2d23837582ad9d51e587b14aeb3
Ben Stoller <stollersystemsound@gmail.com>
2021-01-11 16:13:23 -0800

    test deploy 1

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -9,7 +9,7 @@ function CardItem(props) {
       <li className='cards__item'>
         <div className='cards__item__link' >
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <a target='_blank' href='https://heptagram.vercel.app/'>
+            <a target='_blank' href={props.live}>
               <img src={props.src} alt='Project' className='cards__item__img' />
             </a>
           </figure>
